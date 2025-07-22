f0527cf20d97e4a8dd3ee62381a9f1432cbe1a7e
azam.wahid <azamwahid@outlook.com>
2025-07-23 02:25:58 +0500

    login

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,7 +1,12 @@
-import { hash } from 'bcryptjs'
+import { compare, hash } from 'bcryptjs'
 import express from 'express'
 import User from '../models/User.js'
 import { errorHandler, successHandler } from '../utils/responseHandler.js'
+
+import pkg from 'jsonwebtoken';
+
+
+const { sign, verify } = pkg;
 
 
 export const register = async (req, res) => {
@@ -43,6 +48,49 @@ export const register = async (req, res) => {
     }
 }
 
-export const login = () => {
-    console.log('login')
+export const login = async (req, res) => {
+    try {
+        const { userName, password } = req.body
+
+        if (!userName || !password) {
+            // return errorHandler(res, 400, "missing fields");
+            return errorHandler(res, 400, "missing fields")
+        }
+
+        // const isExists = await Users.find({ $or: [{ email: email }, { userName: userName }] })
+        const isExists = await User.findOne({ userName: userName })
+
+
+        if (!isExists) {
+            return errorHandler(res, 400, "User Name not exists, please retry")
+        }
+        const isPasswordCorrect = await compare(
+            password, isExists.password
+        );
+        if (!isPasswordCorrect) {
+            return errorHandler(res, 400, "User Name not exists, please retry")
+        }
+
+
+
+
+        const token = sign({
+            id: isExists._id,
+            isAdmin: isExists.isAdmin,
+        }, process.env.JWT)
+        console.log(token)
+
+        const { userPassword, ...otherDetails } = isExists._doc;
+        res.cookie("access_token", token, {
+            httpOnly: true,
+            // secure: true,
+            // sameSite: "None"
+        });
+
+        return successHandler(res, 200, "User login Successfully")
+
+    } catch (error) {
+        return errorHandler(res, 400, "soemthing went wroing", error.message)
+
+    }
 }
