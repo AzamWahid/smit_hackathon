613724b4daa7b74a833f605ab357e4148a010d31
azam.wahid <azamwahid@outlook.com>
2025-07-30 03:25:40 +0500

    checking done

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -6,6 +6,7 @@ const userRouter = express.Router();
 
 userRouter.get('/getAllUsers', getAllUsers)
 userRouter.get('/getUserByID/:id', getUserByID)
+userRouter.get('/getSpecificUser', getSpecificUser)
 // userRouter.post('/login', login)
 // userRouter.post('/profilePic', upload.single('ProfPic'), profilePicUpload)
 
