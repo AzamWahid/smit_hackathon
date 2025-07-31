da72b31a332c6d9065962da014729f97b751fee7
Muhammad Azam <AzamWahid@outlook.com>
2025-07-31 20:38:10 +0500

    app bar

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -16,7 +16,7 @@ import AdbIcon from '@mui/icons-material/Adb';
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
-function AppBar() {
+function NavBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
@@ -156,4 +156,4 @@ function AppBar() {
     </AppBar>
   );
 }
-export default AppBar;
+export default NavBar;
